b7d66f7d34bd0d7ed539465ba047c53076fde417
MelaniaRyazanov <melania.ryazanov@gmail.com>
2026-03-23 09:34:59 -0700

    Monster.java

--- Monster.java
+++ Monster.java
@@ -1,5 +1,6 @@
 public class Monster {
 private String monsterType;
+  
    private final String[] monsterTypes = {
             "Goblin",
             "Skeleton Warrior",
@@ -25,14 +26,21 @@ private String monsterType;
 
    public Monster() {
       // TODO: Pick a random monster type from monsterTypes
+      monsterType = getMonsterType();
    }
    
    
    public String getMonsterType() {
+   
+      int monsterLength = monsterTypes.length;
+      int indexOfMonster = (int)(Math.random() * monsterLength);
+      String monsterType = monsterTypes[indexOfMonster];
       return this.monsterType;
    }
    
    public String toString() {
+      return "Monster type: " + monsterType;
       // TODO: Build a descriptive String for use by other methods
    }
+   
 }
